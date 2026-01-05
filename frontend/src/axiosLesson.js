e8114c279b03aae11e72c57c295a29c3700d9c2b
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2026-01-04 20:35:51 -0400

    Update axiosLesson.js

--- frontend/src/axiosLesson.js
+++ frontend/src/axiosLesson.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
 const lessonApi = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: import.meta.env.VITE_API_LESSON_URL,
+})
+
+lessonApi.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
 
 export default lessonApi
